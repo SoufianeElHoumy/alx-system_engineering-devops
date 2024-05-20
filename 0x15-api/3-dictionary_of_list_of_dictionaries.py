5cbcc7a4b4ec748060c50b38ff1e97a3b2c28e36
SoufianeElHoumy <142119581+SoufianeElHoumy@users.noreply.github.com>
2024-05-20 19:15:48 +0000

    API by Soufiane El Houmy

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -33,3 +33,5 @@ if __name__ == '__main__':
             """A little Something"""
     with open('todo_all_employees.json', 'w') as f:
         json.dump(users_dict, f)
+        
+# Newline at the end of the file
