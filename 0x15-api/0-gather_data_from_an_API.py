d38c266c8ff80523f31fbc77726e54010079c034
SoufianeElHoumy <142119581+SoufianeElHoumy@users.noreply.github.com>
2024-05-20 20:00:59 +0000

    API by Soufiane El Houmy

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,13 +5,17 @@ import sys
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employee_id)).json()
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)).json()
+    base_url = "https://jsonplaceholder.typicode.com"
+    user = requests.get(f"{base_url}/users/{employee_id}").json()
+    todos = requests.get(f"{base_url}/todos?userId={employee_id}").json()
 
     done_tasks = [task for task in todos if task.get('completed') is True]
     total_tasks = len(todos)
 
-    print("Employee {} is done with tasks({}/{}):".format(user.get('name'), len(done_tasks), total_tasks))
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get('name'), len(done_tasks), total_tasks))
 
     for task in done_tasks:
         print("\t {}".format(task.get('title')))
+
+# Newline at the end of the file
